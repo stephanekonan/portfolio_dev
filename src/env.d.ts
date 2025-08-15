8e95c136802c6abef60e2919ba6ee1286babdc37
stephane_konan <87937961+stephanekonan@users.noreply.github.com>
2025-08-15 10:52:22 +0000

    add hotjar insights

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+interface Window {
+    hj?: {
+        (...args: any[]): void;
+        q?: any[];
+    };
+    _hjSettings?: {
+        hjid: number;
+        hjsv: number;
+    };
+}
