c7e1701618be360c235d50a59454d7e99517a2b7
stephane_konan <87937961+stephanekonan@users.noreply.github.com>
2025-08-17 20:24:22 +0000

    redirection relative correction

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,13 @@
 import type { MiddlewareHandler } from "astro";
 
 export const onRequest: MiddlewareHandler = async (context, next) => {
-    const lang = context.request.headers.get('accept-language')?.includes('fr') ? 'fr' : 'en';
+    const lang = context.request.headers.get("accept-language")?.includes("fr")
+        ? "fr"
+        : "en";
 
-    if (context.url.pathname === '/') {
-        return Response.redirect(`/${lang}`, 302);
+    if (context.url.pathname === "/") {
+        const redirectUrl = new URL(`/${lang}`, context.url);
+        return Response.redirect(redirectUrl.toString(), 302);
     }
 
     return next();
